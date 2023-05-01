85da8fa98aaf3d1f21208c2474f1f7777b3d0b57
Сергей Истягин <99539900+Fr4t0s@users.noreply.github.com>
2023-05-02 00:46:28 +0300

    Update js.js

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('Всегда лучше быть, чем казаться \u{1F60E}')
+	alert('Всегда лучше быть одним, чем казаться совсем другим \u{1F60E}')
 })
